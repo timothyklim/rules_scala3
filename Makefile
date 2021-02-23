7114edbf01697deb01663d51620ea04c3d9fa818
Timothy Klim <git@timothyklim.com>
2021-02-23 11:50:18 +0700

    Upgrade deps
    Signed-off-by: Timothy Klim <git@timothyklim.com>

--- Makefile
+++ Makefile
@@ -1,3 +1,17 @@
+deps-update:
+	bazel run @annex//:pin
+	bazel run @annex_scalafmt//:pin
+	bazel run @annex_proto//:pin
+
+	cd tests && bazel run @annex_test//:pin
+
+deps-install:
+	bazel run @unpinned_annex//:pin
+	bazel run @unpinned_annex_scalafmt//:pin
+	bazel run @unpinned_annex_proto//:pin
+
+	cd tests && bazel run @unpinned_annex_test//:pin
+
 bzl-lint:
 	find . -type f -iname "*.bazel" -o -iname "*.bzl" -o -iname BUILD -o -iname WORKSPACE | xargs buildifier --lint=warn --warnings=all
 
--- shell.nix
+++ shell.nix
@@ -1,16 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let
-  nixpkgsPinned = import
-    (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/6a2dd6cf4688a5be5f03e34a7c86c02133f6c8a7.tar.gz";
-      sha256 = "09s6jjq5hl06lrmamd43k6i1ayg0fc8ajfx8kr9qh53gyzirslhf";
-    })
-    { };
-in
 with pkgs; pkgs.mkShell {
   name = "rules_scala-env";
-  nativeBuildInputs = [ bash nixpkgsPinned.bazel_4 nixpkgsPinned.bazel-buildtools ];
+  nativeBuildInputs = [ bash bazel_4 bazel-buildtools ];
   buildInputs = [ python2 gnumake ];
 
   shellHook = ''
