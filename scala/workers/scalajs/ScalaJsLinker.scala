24a92cac05666dff00ba4b1e55dc2e981cbc639d
Mikhail Titov <mikhail1207@gmail.com>
2021-12-28 12:20:59 +0300

    Initial support for scalajs

--- scala/workers/scalajs/ScalaJsLinker.scala
+++ scala/workers/scalajs/ScalaJsLinker.scala
@@ -1,79 +1,108 @@
-// Copied from https://github.com/com-lihaoyi/mill/blob/main/scalajslib/src/ScalaJSModule.scala
-
 package rules_scala.workers.scalajs
 
-import java.net.URI
-import java.io.File
-
-import scala.collection.mutable
-import scala.concurrent.{Await, Future}
+import java.nio.file.Path
+import scala.concurrent.Await
 import scala.concurrent.duration.Duration
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.ref.WeakReference
+import concurrent.ExecutionContext.Implicits.global
 
-import org.scalajs.jsenv.{Input, JSEnv, RunConfig}
-import org.scalajs.jsenv.nodejs.NodeJSEnv.SourceMap
-import org.scalajs.linker.{PathIRContainer, PathIRFile, PathOutputFile, StandardImpl}
-import org.scalajs.linker.interface.*
+import org.scalajs.linker.{PathIRContainer, PathOutputFile, StandardImpl}
+import org.scalajs.linker.interface.{LinkerOutput, ModuleInitializer, ModuleKind, Semantics, StandardConfig}
 import org.scalajs.logging.ScalaConsoleLogger
-import org.scalajs.testing.adapter.{TestAdapter, TestAdapterInitializer}
 
 object ScalaJsLinker:
-  final case class LinkerInput(fullOpt: Boolean, moduleKind: ModuleKind, useECMAScript2015: Boolean, dest: File)
 
-  def reuseOrCreate(input: LinkerInput): Linker = cache.get(input) match
-    case Some(WeakReference(linker)) => linker
-    case _ =>
-      val newLinker = createLinker(input)
-      cache.update(input, WeakReference(newLinker))
-      newLinker
+//  private val cache = mutable.Map.empty[LinkerInput, WeakReference[Linker]]
+//
+//  def reuseOrCreate(input: LinkerInput): Linker = cache.get(input) match
+//    case Some(WeakReference(linker)) => linker
+//    case _ =>
+//      val newLinker = createLinker(input)
+//      cache.update(input, WeakReference(newLinker))
+//      newLinker
 
-  private def createLinker(input: LinkerInput): Linker =
-    val semantics = if input.fullOpt then Semantics.Defaults.optimized else Semantics.Defaults
-    val config = StandardConfig()
-      .withClosureCompilerIfAvailable(input.fullOpt)
-      .withESFeatures(_.withUseECMAScript2015(input.useECMAScript2015))
-      .withModuleKind(input.moduleKind)
-      .withOptimizer(input.fullOpt)
-      .withSemantics(semantics)
-    StandardImpl.linker(config)
 
-  private val cache = mutable.Map.empty[LinkerInput, WeakReference[Linker]]
+  def link(args: ScalaJsWorker.Arguments) =
+    val linkerConfig = StandardConfig()
+      .withModuleKind(args.moduleKind)
+      .withOptimizer(true)
+      .withSemantics(Semantics.Defaults.optimized)
+    val linker = StandardImpl.linker(linkerConfig)
 
-  def link(
-      sources: Array[File],
-      libraries: Array[File],
-      dest: File,
-      main: String | Null,
-      testBridgeInit: Boolean,
-      fullOpt: Boolean,
-      moduleKind: ModuleKind,
-      useECMAScript2015: Boolean
-  ): File =
-    val linker = reuseOrCreate(LinkerInput(fullOpt = fullOpt, moduleKind, useECMAScript2015 = useECMAScript2015, dest))
     val cache = StandardImpl.irFileCache().newCache
-    val sourceIRsFuture = Future.sequence(sources.toSeq.map(f => PathIRFile(f.toPath())))
-    val irContainersPairs = PathIRContainer.fromClasspath(libraries.map(_.toPath()))
-    val libraryIRsFuture = irContainersPairs.flatMap(pair => cache.cached(pair._1))
-    val jsFile = dest.toPath()
-    val sourceMap = jsFile.resolveSibling(s"${jsFile.getFileName()}.map")
-    val linkerOutput = LinkerOutput(PathOutputFile(jsFile))
-      .withJSFileURI(URI.create(jsFile.getFileName.toString))
-      .withSourceMap(PathOutputFile(sourceMap))
-      .withSourceMapURI(URI.create(sourceMap.getFileName.toString))
-    val logger = ScalaConsoleLogger()
+    val sourceIRs = PathIRContainer
+      .fromClasspath(args.sourcesAndLibs.toSeq)
+      .flatMap((irContainer, _) => cache.cached(irContainer))
+    val jsFile = args.dest.toPath
+    val output = LinkerOutput(PathOutputFile(jsFile))
+      .withJSFileURI(java.net.URI.create(jsFile.getFileName.toString))
+    val mainInitializer = args.mainClass.map { mainCls =>
+      if args.mainMethodWithArgs then ModuleInitializer.mainMethodWithArgs(mainCls, args.mainMethod)
+      else ModuleInitializer.mainMethod(mainCls, args.mainMethod)
+    }
+    val logger = new ScalaConsoleLogger()
+    val result =
+      for
+        irFiles <- sourceIRs
+        _ <- linker.link(irFiles, mainInitializer.toList, output, logger)
+      yield ()
+    Await.result(result, Duration.Inf)
+end ScalaJsLinker
 
-    val moduleInitializers = Seq.newBuilder[ModuleInitializer]
-    main match
-      case cls: String => moduleInitializers += ModuleInitializer.mainMethodWithArgs(cls, "main")
-      case null        => ()
-    if testBridgeInit then
-      moduleInitializers += ModuleInitializer.mainMethod(TestAdapterInitializer.ModuleClassName, TestAdapterInitializer.MainMethodName)
-
-    val resultFuture = for
-      sourceIRs <- sourceIRsFuture
-      libraryIRs <- libraryIRsFuture
-      _ <- linker.link(sourceIRs ++ libraryIRs, moduleInitializers.result(), linkerOutput, logger)
-    yield dest
-
-    Await.result(resultFuture, Duration.Inf)
+//  final case class LinkerInput(fullOpt: Boolean, moduleKind: ModuleKind, useECMAScript2015: Boolean, dest: File)
+//
+//  def reuseOrCreate(input: LinkerInput): Linker = cache.get(input) match
+//    case Some(WeakReference(linker)) => linker
+//    case _ =>
+//      val newLinker = createLinker(input)
+//      cache.update(input, WeakReference(newLinker))
+//      newLinker
+//
+//  private def createLinker(input: LinkerInput): Linker =
+//    val semantics = if input.fullOpt then Semantics.Defaults.optimized else Semantics.Defaults
+//    val config = StandardConfig()
+//      .withClosureCompilerIfAvailable(input.fullOpt)
+//      .withESFeatures(_.withUseECMAScript2015(input.useECMAScript2015))
+//      .withModuleKind(input.moduleKind)
+//      .withOptimizer(input.fullOpt)
+//      .withSemantics(semantics)
+//    StandardImpl.linker(config)
+//
+//  private val cache = mutable.Map.empty[LinkerInput, WeakReference[Linker]]
+//
+//  def link(
+//      sources: Array[File],
+//      libraries: Array[File],
+//      dest: File,
+//      main: String | Null,
+//      testBridgeInit: Boolean,
+//      fullOpt: Boolean,
+//      moduleKind: ModuleKind,
+//      useECMAScript2015: Boolean
+//  ): File =
+//    val linker = reuseOrCreate(LinkerInput(fullOpt = fullOpt, moduleKind, useECMAScript2015 = useECMAScript2015, dest))
+//    val cache = StandardImpl.irFileCache().newCache
+//    val sourceIRsFuture = Future.sequence(sources.toSeq.map(f => PathIRFile(f.toPath())))
+//    val irContainersPairs = PathIRContainer.fromClasspath(libraries.map(_.toPath()))
+//    val libraryIRsFuture = irContainersPairs.flatMap(pair => cache.cached(pair._1))
+//    val jsFile = dest.toPath()
+//    val sourceMap = jsFile.resolveSibling(s"${jsFile.getFileName()}.map")
+//    val linkerOutput = LinkerOutput(PathOutputFile(jsFile))
+//      .withJSFileURI(URI.create(jsFile.getFileName.toString))
+//      .withSourceMap(PathOutputFile(sourceMap))
+//      .withSourceMapURI(URI.create(sourceMap.getFileName.toString))
+//    val logger = ScalaConsoleLogger()
+//
+//    val moduleInitializers = Seq.newBuilder[ModuleInitializer]
+//    main match
+//      case cls: String => moduleInitializers += ModuleInitializer.mainMethodWithArgs(cls, "main")
+//      case null        => ()
+//    if testBridgeInit then
+//      moduleInitializers += ModuleInitializer.mainMethod(TestAdapterInitializer.ModuleClassName, TestAdapterInitializer.MainMethodName)
+//
+//    val resultFuture = for
+//      sourceIRs <- sourceIRsFuture
+//      libraryIRs <- libraryIRsFuture
+//      _ <- linker.link(sourceIRs ++ libraryIRs, moduleInitializers.result(), linkerOutput, logger)
+//    yield dest
+//
+//    Await.result(resultFuture, Duration.Inf)
--- scala/workers/scalajs/ScalaJsWorker.scala
+++ scala/workers/scalajs/ScalaJsWorker.scala
@@ -6,33 +6,38 @@ import java.nio.file.{Files, Path, Paths}
 import org.scalajs.linker.interface.ModuleKind
 import scopt.OParser
 
+import rules_scala.workers.common.Bazel
 import rules_scala.common.worker.WorkerMain
 
-object ScalaJsWorker extends WorkerMain[ScalaJsWorker.Arguments]:
-  override def init(args: collection.Seq[String]): Arguments =
-    Arguments(args).getOrElse(throw IllegalArgumentException(s"init args is invalid: ${args.mkString(" ")}"))
-
-  override def work(workerArgs: Arguments, args: collection.Seq[String]): Unit =
-    ???
-
+object ScalaJsWorker: // extends WorkerMain[ScalaJsWorker.Arguments]:
+//  override def init(args: collection.Seq[String]): Arguments =
+//    Arguments(args).getOrElse(throw IllegalArgumentException(s"init args is invalid: ${args.mkString(" ")}"))
+//
+//  override def work(workerArgs: Arguments, args: collection.Seq[String]): Unit =
+//    ???
+//
+  def main(args: Array[String]): Unit =
+    val runArgs = Arguments(Bazel.parseParams(args)).getOrElse(throw IllegalArgumentException(s"args is invalid: ${args.mkString(" ")}"))
+    ScalaJsLinker.link(runArgs)
+    
   final case class Arguments(
-      fullOpt: Boolean = false,
+      mainClass: Option[String] = None,
+      mainMethod: String = "main",
+      mainMethodWithArgs: Boolean = true,
+      sourcesAndLibs: Vector[Path] = Vector.empty,
+      dest: File = File("."),
       moduleKind: ModuleKind = ModuleKind.NoModule,
-      ecma2015: Boolean = false,
-      dest: File = new File("."),
-      classpath: Vector[Path] = Vector.empty,
-      sourceJars: Vector[Path] = Vector.empty,
-      sources: Vector[File] = Vector.empty,
   )
   object Arguments:
     private val builder = OParser.builder[Arguments]
     import builder.*
 
     private val parser = OParser.sequence(
-      opt[Boolean]("full_opt").action((o, c) => c.copy(fullOpt = o)),
-      opt[Boolean]("ecma2015").action((o, c) => c.copy(ecma2015 = o)),
+      opt[Option[String]]("main-class").optional().action((o, c) => c.copy(mainClass = o)),
+      opt[String]("main-method").optional().action((o, c) => c.copy(mainMethod = o)),
+      opt[Boolean]("with-args").optional().action((o, c) => c.copy(mainMethodWithArgs = o)),
       opt[File]("dest").required().action((f, c) => c.copy(dest = f)),
-      opt[String]("module").action((m, c) =>
+      opt[String]("module").optional().action((m, c) =>
         c.copy(moduleKind = m match
           case "no"     => ModuleKind.NoModule
           case "common" => ModuleKind.CommonJSModule
@@ -40,21 +45,11 @@ object ScalaJsWorker extends WorkerMain[ScalaJsWorker.Arguments]:
           case module   => throw IllegalArgumentException(s"Unknown module: $module. Valid modules: no, common, es")
         )
       ),
-      opt[File]("cp")
+      arg[File]("<file>...")
+        .required()
         .unbounded()
-        .optional()
-        .action((cp, c) => c.copy(classpath = c.classpath :+ cp.toPath))
-        .text("Compilation classpath"),
-      opt[File]("source_jar")
-        .unbounded()
-        .optional()
-        .action((s, c) => c.copy(sourceJars = c.sourceJars :+ s.toPath()))
-        .text("Source jars"),
-      arg[File]("<source>...")
-        .unbounded()
-        .optional()
-        .action((s, c) => c.copy(sources = c.sources :+ s))
-        .text("Source files")
+        .action((cp, c) => c.copy(sourcesAndLibs = c.sourcesAndLibs :+ cp.toPath))
+        .text("Files with source archives and libraries"),
     )
 
     def apply(args: collection.Seq[String]): Option[Arguments] = OParser.parse(parser, args, Arguments())
