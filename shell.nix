9237ef73348d02f0f5538a86291f52555cdbe269
Timothy Klim <git@timothyklim.com>
2023-09-16 07:48:44 +0400

    fix: macOS

--- shell.nix
+++ shell.nix
@@ -2,9 +2,11 @@
 
 with pkgs;
 
-mkShell {
+let
+  stdenv = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.llvmPackages_16.stdenv else pkgs.stdenv;
+in mkShell.override { inherit stdenv; } {
   name = "rules_scala-env";
   nativeBuildInputs = [ jdk ];
   buildInputs = [ bash bazel bazel-buildtools python3 just fd nixpkgs-fmt ];
-  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+  LOCALE_ARCHIVE = lib.optionals stdenv.isLinux "${glibcLocales}/lib/locale/locale-archive";
 }
