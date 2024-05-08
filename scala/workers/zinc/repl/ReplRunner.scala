cb41b9e653243c652137fa902e680601495f110a
Timothy Klim <git@timothyklim.com>
2024-05-08 11:58:41 +0700

    chore: cleanup

--- scala/workers/zinc/repl/ReplRunner.scala
+++ scala/workers/zinc/repl/ReplRunner.scala
@@ -1,19 +1,18 @@
 package rules_scala
 package workers.zinc.repl
 
-import workers.common.{AnnexLogger, AnnexScalaInstance, Bazel, FileUtil, LogLevel, TopClassLoader}
-
 import java.io.File
 import java.nio.file.{Files, Path, Paths}
 import java.net.URLClassLoader
-import java.util.Collections
 
 import scala.jdk.CollectionConverters.*
+import scala.language.unsafeNulls
 
 import sbt.internal.inc.{PlainVirtualFile, PlainVirtualFileConverter, ZincUtil}
 import sbt.internal.inc.classpath.ClassLoaderCache
 import scopt.OParser
-import xsbti.Logger
+
+import workers.common.{AnnexLogger, AnnexScalaInstance, Bazel, LogLevel, TopClassLoader}
 
 final case class ReplRunnerArguments(
     logLevel: LogLevel = LogLevel.Warn
