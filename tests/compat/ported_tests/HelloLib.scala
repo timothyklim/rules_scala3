d904111e2334a1553626d772b8925e0a5970c038
anna-skrodzka <annaskrod@h=gmail.com>
2024-11-05 11:44:16 +0100

    compat tests fixed (almost)

--- tests/compat/ported_tests/HelloLib.scala
+++ tests/compat/ported_tests/HelloLib.scala
@@ -2,12 +2,11 @@ package scala.test
 
 object HelloLib:
 
-  def printMessage(arg: String) {
+  def printMessage(arg: String) = 
     MacroTest.hello(arg == "yo")
     println(getOtherLibMessage(arg))
     println(getOtherJavaLibMessage(arg))
     println(Exported.message)
-  }
 
   def getOtherLibMessage(arg: String): String =
     arg + " " + OtherLib.getMessage()
