be068980f311d65987c00163c50d308481718c4c
Anna Skrodzka <56291558+anna-skrodzka@users.noreply.github.com>
2024-11-19 01:45:39 +0100

    Add bazel-flake (#57)
    * update nix files
    
    * BUILD file updated
    
    * jmh test updated
    
    * test-frameworks/mixed updated
    
    * scaladoc test deleted (project doesn't have //mockutil:mocklib)
    
    ---------
    
    Co-authored-by: anna-skrodzka <annaskrod@h=gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
-  description = "Bazel rules_scala flake";
+  description = "Bazel rules_scala3 flake";
 
   inputs = {
     nixpkgs.url = "nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    bazel.url = "git+ssh://git@github.com/timothyklim/bazel-flake.git";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, bazel }:
     with flake-utils.lib; with system; eachSystem [ aarch64-darwin aarch64-linux x86_64-linux ] (system:
       let
         pkgs = import nixpkgs {
@@ -20,7 +21,7 @@
         };
       in
       rec {
-        devShell = pkgs.callPackage ./shell.nix { };
+        devShell = pkgs.callPackage ./shell.nix { bazel = bazel.packages.${system}.default; };
         formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
       });
 }
--- tests/scaladoc/A.scala
+++
@@ -1,7 +0,0 @@
-import mockutil.MockLib
-
-/** A Scaladoc
-  */
-object A:
-  def a = B
-  def addOne(i: Int) = MockLib.add(i, 1)
